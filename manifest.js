e44cb27edc55c1bae602196af903e1b9ec39f529
pbxgit <165305311+pbxgit@users.noreply.github.com>
2025-08-31 12:42:49 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Cineverse",
+    "short_name": "Cineverse",
+    "start_url": ".",
+    "display": "standalone",
+    "background_color": "#0A0F1E",
+    "theme_color": "#0A0F1E",
+    "description": "A futuristic, AI-powered personal movie explorer.",
+    "icons": [
+        {
+            "src": "/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
