6a1b80a3b92f294ec7ec2619635d43a17ca737cd
pbxgit <165305311+pbxgit@users.noreply.github.com>
2025-08-31 12:42:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// CINEVERSE - Main Application Logic
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to Cineverse!");
+    // We will fetch movies and initialize the app here
+});
