be07fea1cfed3f99af7a5b35d3aa6c01a6feae6a
pbxgit <165305311+pbxgit@users.noreply.github.com>
2025-09-04 00:32:49 +0530

    Update tmdb.js

--- functions/tmdb.js
+++ functions/tmdb.js
@@ -1,24 +1,19 @@
+// functions/tmdb.js
 exports.handler = async function(event) {
-    const { endpoint, queryParams } = event.queryStringParameters;
-    const apiKey = process.env.TMDB_API_KEY;
-    
-    if (!apiKey) {
-        return { statusCode: 500, body: 'API key not configured.' };
+    const { endpoint, params } = event.queryStringParameters;
+    const { TMDB_API_KEY } = process.env;
+
+    if (!TMDB_API_KEY) {
+        return { statusCode: 500, body: 'TMDB API Key not found.' };
     }
 
-    const apiUrl = `https://api.themoviedb.org/3${endpoint}?api_key=${apiKey}&${queryParams}`;
+    const url = `https://api.themoviedb.org/3${endpoint}?api_key=${TMDB_API_KEY}&${params}`;
 
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(url);
         const data = await response.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data),
-        };
+        return { statusCode: 200, body: JSON.stringify(data) };
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch data from TMDb' }),
-        };
+        return { statusCode: 500, body: JSON.stringify({ error: 'Failed to fetch data' }) };
     }
 };
