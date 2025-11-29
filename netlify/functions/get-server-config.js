fd9962165d53e6bcf95b474ffd40d84afc89107f
pbxgit <165305311+pbxgit@users.noreply.github.com>
2025-11-29 14:16:32 +0530

    Add server configuration handler for Netlify functions

---
+++ netlify/functions/get-server-config.js
@@ -0,0 +1,39 @@
+exports.handler = async function(event, context) {
+    // --- THIS IS YOUR CONTROL PANEL ---
+    // Edit this list to add/remove/reorder buttons.
+    // The top item will be the default selected server.
+    
+    const serverList = [
+        {
+            name: "Videasy",
+            // You can use standard text for the icon if you don't want to find SVG codes
+            // Example: "icon": "🎬", 
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>`,
+            movieUrlTemplate: "https://vidsrc.to/embed/movie/{id}",
+            tvUrlTemplate: "https://vidsrc.to/embed/tv/{id}/{season}/{episode}"
+        },
+        {
+            name: "Vidfast",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"></path></svg>`,
+            movieUrlTemplate: "https://vidfast.pro/movie/{id}?theme=EF4444&autoPlay=true",
+            tvUrlTemplate: "https://vidfast.pro/tv/{id}/{season}/{episode}?theme=EF4444&autoPlay=true"
+        },
+        {
+            name: "CinemaOS",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10 3.5a1.5 1.5 0 011.5 1.5v2.879a1.5 1.5 0 01-.379 1.06L9.439 12.12a1.5 1.5 0 01-2.121 0l-2.18-2.18a1.5 1.5 0 010-2.122L8.939 4.02a1.5 1.5 0 011.06-.521H10z"></path><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12z"></path></svg>`,
+            movieUrlTemplate: "https://cinemaos.tech/player/{id}",
+            tvUrlTemplate: "https://cinemaos.tech/player/{id}/{season}/{episode}"
+        }
+    ];
+
+    // --- END CONTROL PANEL ---
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'public, max-age=3600' // Cache this for 1 hour so it loads fast
+        },
+        body: JSON.stringify(serverList)
+    };
+};
