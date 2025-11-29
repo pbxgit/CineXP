8fd7c616384e39fc7c2d1cd6611de6ab9a2e5582
pbxgit <165305311+pbxgit@users.noreply.github.com>
2025-11-29 14:29:13 +0530

    Replace server configurations and update URLs

--- netlify/functions/get-server-config.js
+++ netlify/functions/get-server-config.js
@@ -5,12 +5,17 @@ exports.handler = async function(event, context) {
     
     const serverList = [
         {
-            name: "Videasy",
-            // You can use standard text for the icon if you don't want to find SVG codes
-            // Example: "icon": "🎬", 
-            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>`,
-            movieUrlTemplate: "https://vidsrc.to/embed/movie/{id}",
-            tvUrlTemplate: "https://vidsrc.to/embed/tv/{id}/{season}/{episode}"
+            name: "VidKing",
+            // Crown Icon
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1.323l3.954 1.582 1.599-.8a1 1 0 01.894 1.79l-1.233.616 1.738 5.42a1 1 0 01-.285 1.05A3.989 3.989 0 0115 15a3.989 3.989 0 01-2.667-1.019 1 1 0 01-.285-1.05l1.715-5.349L11 6.477V16h2a1 1 0 110 2H7a1 1 0 110-2h2V6.477L6.237 7.582l1.715 5.349a1 1 0 01-.285 1.05A3.989 3.989 0 015 15a3.989 3.989 0 01-2.667-1.019 1 1 0 01-.285-1.05l1.738-5.42-1.233-.616a1 1 0 01.894-1.79l1.599.8L9 4.323V3a1 1 0 011-1z" clip-rule="evenodd" /></svg>`,
+            movieUrlTemplate: "https://www.vidking.net/embed/movie/{id}?color=e50914&autoPlay=true&nextEpisode=true&episodeSelector=true",
+            tvUrlTemplate: "https://www.vidking.net/embed/tv/{id}/{season}/{episode}?color=e50914&autoPlay=true&nextEpisode=true&episodeSelector=true"
+        },
+        {
+            name: "CinemaOS",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10 3.5a1.5 1.5 0 011.5 1.5v2.879a1.5 1.5 0 01-.379 1.06L9.439 12.12a1.5 1.5 0 01-2.121 0l-2.18-2.18a1.5 1.5 0 010-2.122L8.939 4.02a1.5 1.5 0 011.06-.521H10z"></path><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12z"></path></svg>`,
+            movieUrlTemplate: "https://cinemaos.tech/player/{id}",
+            tvUrlTemplate: "https://cinemaos.tech/player/{id}/{season}/{episode}"
         },
         {
             name: "Vidfast",
@@ -19,10 +24,10 @@ exports.handler = async function(event, context) {
             tvUrlTemplate: "https://vidfast.pro/tv/{id}/{season}/{episode}?theme=EF4444&autoPlay=true"
         },
         {
-            name: "CinemaOS",
-            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10 3.5a1.5 1.5 0 011.5 1.5v2.879a1.5 1.5 0 01-.379 1.06L9.439 12.12a1.5 1.5 0 01-2.121 0l-2.18-2.18a1.5 1.5 0 010-2.122L8.939 4.02a1.5 1.5 0 011.06-.521H10z"></path><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12z"></path></svg>`,
-            movieUrlTemplate: "https://cinemaos.tech/player/{id}",
-            tvUrlTemplate: "https://cinemaos.tech/player/{id}/{season}/{episode}"
+            name: "Videasy",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>`,
+            movieUrlTemplate: "https://vidsrc.to/embed/movie/{id}",
+            tvUrlTemplate: "https://vidsrc.to/embed/tv/{id}/{season}/{episode}"
         }
     ];
 
